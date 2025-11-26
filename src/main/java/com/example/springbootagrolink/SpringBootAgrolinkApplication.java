0811d7edce9578ece566a724756d8891b723c5aa
NicolasRengifo24 <nicolaseggo@gmail.com>
2025-11-26 11:37:08 -0500

    primera carga

---
+++ src/main/java/com/example/springbootagrolink/SpringBootAgrolinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootagrolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAgrolinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAgrolinkApplication.class, args);
+    }
+
+}
