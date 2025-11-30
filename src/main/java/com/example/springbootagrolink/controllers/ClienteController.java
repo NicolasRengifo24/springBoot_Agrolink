f39c1e8dc708a28c9798c85f898caa7e521a97c2
Samuel1000-90 <samueldg0210@gmail.com>
2025-11-29 23:56:32 -0500

    SubidaNuevosArchivos Services y Controller

---
+++ src/main/java/com/example/springbootagrolink/controllers/ClienteController.java
@@ -0,0 +1,4 @@
+package com.example.springbootagrolink.controllers;
+
+public class ClienteController {
+}
---
+++ src/main/java/com/example/springbootagrolink/services/MaquinaService.java
@@ -0,0 +1,4 @@
+package com.example.springbootagrolink.services;
+
+public class MaquinaService {
+}
